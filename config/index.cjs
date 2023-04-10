9f0680933c8a30d72cc31ba7b7bda5af8c638c0b
IDKHugh <98767047+IDKHugh@users.noreply.github.com>
2023-04-11 03:59:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6hU36josZlPx2U2yU7-rNf1p1pI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1pQ3saBJpMNDvmi09dvfteozfyjgcgKXtSsIPzj7eP4',
+      useTemplateId: '8VBvTrmWnN5ZBbrd4py6V-mloCx5GUtsWLPnXCFu7gs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-21',
       festivals: [
