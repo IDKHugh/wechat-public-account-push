cd55b8036d9c2eb764784c05053ced1541d47166
IDKHugh <98767047+IDKHugh@users.noreply.github.com>
2023-04-11 14:33:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '思漩babe',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o6hU36josZlPx2U2yU7-rNf1p1pI',
+      id: 'o6hU36kxcDlvAepJj5pL4WzYd3vM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '8VBvTrmWnN5ZBbrd4py6V-mloCx5GUtsWLPnXCFu7gs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
